c68ce4501135b17406ce3a40532c8435f229388a
Nikhil <biragenikhil@gmail.com>
2020-09-29 10:09:22 +0530

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MedicineListComponent } from './medicine-list/medicine-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { ManageMedicineComponent } from './manage-medicine/manage-medicine.component';
+import { MedicineDetailsComponent } from './medicine-details/medicine-details.component';
+
+const routes = [
+  { path: 'medicines', component: MedicineListComponent },
+  { path: 'add-medicines', component: ManageMedicineComponent },
+  { path: 'edit-medicines/:id', component: ManageMedicineComponent },
+  { path: 'medicines/:id', component: MedicineDetailsComponent },
+  { path: '', redirectTo: '/medicines', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageMedicineComponent,
+    MedicineDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/models/medicine.model.ts
@@ -0,0 +1,9 @@
+export interface IMedicine {
+    id: number;
+    name: string;
+    brand: string;
+    notes: string;
+    price: number;
+    quantity: number;
+    expiryDate: Date
+}
